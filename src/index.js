7d82369633203a8f09e0dfeb7ebd85975a249966
Kenji <kenji.au9@gmail.com>
2020-09-17 13:47:48 -0600

    Added HelloKenji component;

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import HelloKenji from './component/HelloKenji';
 import './index.css';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <> </>
+    <HelloKenji />
   </React.StrictMode>,
   document.getElementById('root')
 );
