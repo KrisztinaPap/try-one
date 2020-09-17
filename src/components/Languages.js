ebc67c28e8da7c3bd161ba6049ae3ffd4d758412
dhmec <dharmoradiya@gmail.com>
2020-09-17 13:55:13 -0600

    Added component to index.js

--- src/components/Languages.js
+++ src/components/Languages.js
@@ -3,12 +3,15 @@ import React from 'react';
 function Languages ()
 {
     return(
-        <>
+        <>  
+            <p>Languages We Learn So Far!</p>
             <ul>
-                <li>
-                    
-                </li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
             </ul>
         </>
     );
+
 }
+export default Languages;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Languages from './components/Languages';
 import './index.css';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <> </>
+    <Languages />
   </React.StrictMode>,
   document.getElementById('root')
 );
