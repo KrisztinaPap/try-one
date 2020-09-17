12274c51cc144fd2d9996b556ba57573413f979b
Krisztina Pap <krisztina.pap@outlook.com>
2020-09-17 13:46:31 -0600

    Added HelloKrisztina component;

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import HelloKrisztina from './component/HelloKrisztina';
 import './index.css';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <> </>
+    <HelloKrisztina />
   </React.StrictMode>,
   document.getElementById('root')
 );
